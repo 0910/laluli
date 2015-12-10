1aae55232724f66b6fa37ff43f9601d95ba5d69c
0910 <e@nuevediez.com.ar>
2015-12-10 10:15:56 -0300

    deployment configs

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,30 +1,57 @@
 server '40.76.56.133', roles: [:web, :app, :db], primary: true
-set :domain, "laluli.nuevediez.com"
-set :repo_url,        'git@github.com:0910/laluli.git'
+
 set :application,     'laluli'
 set :user,            'ubuntu'
-set :rbenv_ruby, '2.2.3'
-
-set :pty,             true
-set :use_sudo,        false
-set :stage,           :production
-set :rails_env,       :production
+set :deploy_to,       "/home/#{user}/applications/#{application}"
 set :deploy_via,      :remote_cache
-set :deploy_to,       "/home/ubuntu/applications/#{fetch(:application)}"
-set :ssh_options,     { forward_agent: true, user: fetch(:user), keys: %w(~/.ssh/id_rsa.pub) }
-set :assets_roles, [:web, :app]            # Defaults to [:web]
-# Default value for linked_dirs is []
-set :linked_dirs, fetch(:linked_dirs, []).push('log', 'shared/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
-set :linked_files, fetch(:linked_files, []).push('config/database.yml', 'config/secrets.yml')
+set :use_sudo,        false
 
 set :scm, :git
+set :repo_url,        'git@github.com:0910/laluli.git'
 set :branch, :master
 set :format, :pretty
 set :log_level, :debug
 set :keep_releases, 5
 
+set :rbenv_ruby, '2.2.3'
+
+set :stage,           :production
+set :rails_env,       :production
+
+
+default_run_options[:pty] = true
+ssh_options[:forward_agent] = true
+set :assets_roles, [:web, :app]            # Defaults to [:web]
+# Default value for linked_dirs is []
+set :linked_dirs, fetch(:linked_dirs, []).push('log', 'shared/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
+set :linked_files, fetch(:linked_files, []).push('config/database.yml', 'config/secrets.yml')
+
+
+after "deploy", "deploy:cleanup" # keep only the last 5 releases
 
 namespace :deploy do
+
+  %w[start stop restart].each do |command|
+    desc "#{command} unicorn server"
+    task command, roles: :app, except: {no_release: true} do
+      run "/etc/init.d/unicorn_#{application} #{command}"
+    end
+  end
+
+  task :setup_config, roles: :app do
+    sudo "ln -nfs #{current_path}/config/nginx.conf /etc/nginx/sites-enabled/#{application}"
+    sudo "ln -nfs #{current_path}/config/unicorn_init.sh /etc/init.d/unicorn_#{application}"
+    run "mkdir -p #{shared_path}/config"
+    put File.read("config/database.yml"), "#{shared_path}/config/database.yml"
+    puts "Now edit the config files in #{shared_path}."
+  end
+  after "deploy:setup", "deploy:setup_config"
+
+  task :symlink_config, roles: :app do
+    run "ln -nfs #{shared_path}/config/database.yml #{release_path}/config/database.yml"
+  end
+  after "deploy:finalize_update", "deploy:symlink_config"
+
   desc "Make sure local git is in sync with remote."
   task :check_revision do
     on roles(:app) do
@@ -35,26 +62,6 @@ namespace :deploy do
       end
     end
   end
+  before "deploy", "deploy:check_revision"
 
-  desc 'Initial Deploy'
-  task :initial do
-    on roles(:app) do
-      before 'deploy:restart'
-      invoke 'deploy'
-    end
-  end
-
-  desc 'Restart application'
-  task :restart do
-    on roles(:app), in: :sequence, wait: 5 do
-      sudo 'service unicorn_laluli restart'
-      sudo 'service nginx restart'
-    end
-  end
-
-
-  before :starting,     :check_revision
-  after  :finishing,    :compile_assets
-  after  :finishing,    :cleanup
-  after  :finishing,    :restart
 end
--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,41 +1,19 @@
 # set path to application
-app_root = "/home/ubuntu/applications/laluli"
-current_path = "/home/ubuntu/applications/laluli/current"
+app_root = "/home/ubuntu/applications/laluli/current"
 shared_path = "/home/ubuntu/applications/laluli/shared"
-working_directory current_path
+working_directory app_root
 
+# Logging
+stderr_path "#{shared_path}/log/unicorn.stderr.log"
+stdout_path "#{shared_path}/log/unicorn.stdout.log"
+
+# Set up socket location
+listen "/tmp/unicorn.laluli.sock"
 
 # Set unicorn options
 worker_processes 2
 preload_app true
 timeout 30
 
-# Set up socket location
-listen "/tmp/unicorn.laluli.sock"
-
-# Logging
-stderr_path "#{shared_path}/log/unicorn.stderr.log"
-stdout_path "#{shared_path}/log/unicorn.stdout.log"
-
 # Set master PID location
-pid "#{current_path}/tmp/pids/unicorn.pid"
-
-before_exec do |server| 
-  ENV["BUNDLE_GEMFILE"] = "#{current_path}/Gemfile" 
-end 
-
-before_fork do |server, worker|
-  defined?(ActiveRecord::Base) and ActiveRecord::Base.connection.disconnect!
-  old_pid = "#{server.config[:pid]}.oldbin"
-  if File.exists?(old_pid) && server.pid != old_pid
-    begin
-      Process.kill("QUIT", File.read(old_pid).to_i)
-    rescue Errno::ENOENT, Errno::ESRCH
-    end
-  end
-end
-
-after_fork do |server, worker|
-  ActiveRecord::Base.establish_connection
-  ActiveRecord::Base.verify_active_connections!
-end
+pid "#{root}/tmp/pids/unicorn.pid"
