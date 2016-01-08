1f227a8ef2f40184cdbd7eb88046fb8d3df3c53a
0910 <e@nuevediez.com.ar>
2016-01-07 21:44:32 -0300

    rio changes

--- app/controllers/stylings_controller.rb
+++ app/controllers/stylings_controller.rb
@@ -5,15 +5,12 @@ class StylingsController < ApplicationController
   	@styling = Styling.find(params[:id])
   end
   def lookbooks
-    @lookbook = true
   	@stylings = Styling.where('category_id = 1')
   end
   def performances
-    @performances = true
   	@stylings = Styling.where('category_id = 2')
   end
   def personal
-    @personal = true
   	@styling = Styling.find(params[:id])
   end
 end
